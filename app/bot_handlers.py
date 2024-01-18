433a72a5c210d22e640dd97d74b6816514142f47
SKB-tg <120108953+SKB-tg@users.noreply.github.com>
2024-01-19 01:06:36 +0300

    Update bot_handlers.py

--- app/bot_handlers.py
+++ app/bot_handlers.py
@@ -410,7 +410,8 @@ async def get_vakancy_handler(request: Request):
 
         res = requests.post(url2, headers=headers1, json=json.loads(data2))
         print(177, res.text)
-        out_txt = eval(res.text)
+        res_j = json.dumps(res.json())
+        out_txt = eval(res_j)
     except requests.exceptions.HTTPError as HTTPError:
         #print(375, 177, res.text)
         return json_response({"ok": False, "data": res.status_code})
